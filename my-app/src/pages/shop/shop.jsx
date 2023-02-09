37947f5f3431c4563be25c7653ecf9509f3b2f5e
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-09 11:38:34 +0200

    making shop and cart pages

---
+++ my-app/src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1>Space Shop</h1>
+        </div>
+        <div className='products'>
+
+        </div>
+    </div>
+  )
+}
