b06576612801c6e29854b4e7abf3c0d63e27c2a9
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-10 12:52:19 +0200

    changing item count from cart

--- my-app/src/context/shop-context.jsx
+++ my-app/src/context/shop-context.jsx
@@ -24,7 +24,11 @@ export const ShopContextProvider = (props) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId]-1}))
     };
 
-    const contexValue = {cartItems, addToCart, removeFromCart};
+    const updateCartItemCount = (newAmount, itemId) =>{
+        setCartItems((prev) => ({...prev, [itemId]: newAmount}))
+    };
+
+    const contexValue = {cartItems, addToCart, removeFromCart,updateCartItemCount };
 
 
     
--- my-app/src/pages/cart/cart-item.jsx
+++ my-app/src/pages/cart/cart-item.jsx
@@ -4,7 +4,7 @@ import './cart.css'
 
 export const CartItem = (props) => {
     const { id, productName, price, productImage } = props.data
-    const { cartItems, addToCart, removeFromCart } = useContext(ShopContext)
+    const { cartItems, addToCart, removeFromCart,updateCartItemCount } = useContext(ShopContext)
     return (
         <div className='cartItem'>
             <img src={productImage} />
@@ -15,7 +15,7 @@ export const CartItem = (props) => {
                 <p>${price}</p>
                 <div className='countHandler'>
                     <button onClick={() => addToCart(id)}>+</button>
-                    <input value={cartItems[id]} />
+                    <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value),id)} />
                     <button onClick={() => removeFromCart(id)} >-</button>
                 </div>
             </div>
